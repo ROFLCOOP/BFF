29f7878b515fb693a8f6aaeca98f7d0ac143aa18
DesignSting <39390336+DesignSting@users.noreply.github.com>
2019-05-22 15:24:45 +1000

    - Here comes mine!

---
+++ BFF/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject creditPanel;
+
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+        Debug.Log("This is the Quit Button");
+    }
+
+    public void CreditButton()
+    {
+        creditPanel.SetActive(true);
+    }
+
+    public void BackButton()
+    {
+        creditPanel.SetActive(false);
+    }
+
+    public void MenuButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ BFF/Assets/Scripts/ResultScreen.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultScreen : MonoBehaviour
+{
+
+    public Text totalTime;
+    public Text totalEnemies;
+    public Text totalShots;
+    public Text totalAccuracy;
+
+    private int shotsFired;
+    private int enemiesKilled;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EndResults();
+
+    }
+
+    public void IsDead()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+    public float TotalTime()
+    {
+        return 0.1f;
+    }
+
+    public int TotalEnemies()
+    {
+        enemiesKilled = 0;
+        return enemiesKilled;
+    }
+
+    public int TotalShots()
+    {
+        shotsFired = 0;
+        return shotsFired;
+    }
+
+    public float TotalAccuracy()
+    {
+        return (shotsFired / enemiesKilled);
+    }
+
+    public void EndResults()
+    {
+
+    }
+}
