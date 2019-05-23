553078548d2352d1d5d01138062017c53b8d60eb
Andrew Kenny <andrewckenny97@gmail.com>
2019-05-23 11:43:42 +1000

    Optimise Character Shot
    - raycasts destroy objects on hit
    - shot properties are easily modifiable

--- BFF/Assets/Scripts/CharacterControl.cs
+++ BFF/Assets/Scripts/CharacterControl.cs
@@ -4,9 +4,34 @@ using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
+    [Tooltip("How fast should the player move('units'/second)")]
     [Range(1, 100)]
-    public int speed = 10;
-    public GameObject m_projectile;
+    public int playerSpeed = 10;
+
+    [Tooltip("How long should the shot take (seconds)")]
+    [Range(1, 10)]
+    public float shotTime = 5;
+
+    [Tooltip("How far should the shot go")]
+    [Range(1, 10)]
+    public float shotDistance = 1;
+
+    [Tooltip("How wide should the opening of the shot be")]
+    [Range(0, 1)]
+    public float shotStartWidth = 0;
+
+    [Tooltip("How wide should the shot be (Degrees)")]
+    [Range(1, 180)]
+    public float shotEndWidth = 45;
+
+    [Tooltip("How many rays are cast per shot, more means less of a gap between rays")]
+    [Range(3, 50)]
+    public int shotRays = 5;
+
+    private float shotCountDown = 0;
+
+    Vector3 shotOriginPoint; //set every time the player shoots
+    Vector3 shotDirection;
 
     // Start is called before the first frame update
     void Start()
@@ -23,9 +48,12 @@ public class CharacterControl : MonoBehaviour
         float angle = Mathf.Atan2(aimDir.x, aimDir.y) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.up);
 
-        if(Input.GetMouseButtonDown(0))
-            shoot();
-
+        if (Input.GetMouseButtonDown(0) && shotCountDown <= 0)
+        {
+            shotCountDown = shotTime;
+            shotOriginPoint = transform.position + transform.forward * 0.5f;
+            shotDirection = transform.forward;
+        }
         if(transform.position != prevPos)
         {
             GameObject cam = GameObject.Find("Main Camera");
@@ -34,60 +62,58 @@ public class CharacterControl : MonoBehaviour
             cam.transform.position = temp;
 
         }
-
-        Vector3 dir = transform.forward;
-
-        Debug.DrawLine(transform.position + (transform.forward * 0.5f), transform.position + transform.forward * 0.5f + dir * 5 , Color.green);
-        dir = Quaternion.AngleAxis(22.5f, Vector3.up) * dir;
-        Debug.DrawLine(transform.position + (transform.forward * 0.5f), transform.position + transform.forward * 0.5f + dir * 5.4f, Color.green);
-        dir = Quaternion.AngleAxis(-45, Vector3.up) * dir;
-        Debug.DrawLine(transform.position + (transform.forward * 0.5f), transform.position + transform.forward * 0.5f + dir * 5.4f, Color.green);
+        
+        if (shotCountDown > 0) // if shot needs to be based on time, put shoot in here
+        {
+            Vector3 originPoint = shotOriginPoint + ((-Vector3.right * shotStartWidth) * 0.5f);
+            Vector3 dir = shotDirection;
+            dir = Quaternion.AngleAxis(-shotEndWidth * 0.5f, Vector3.up) * dir;
+            for (int i = 0; i < shotRays; i++)
+            {
+                Debug.DrawLine(originPoint + Vector3.up * 0.1f, (originPoint + Vector3.up * 0.1f) + dir * (shotDistance  * ((shotTime - shotCountDown) / shotTime)), Color.red);
+                Debug.DrawLine(originPoint, originPoint + dir * shotDistance, Color.blue);
+                dir = Quaternion.AngleAxis(shotEndWidth / (shotRays - 1), Vector3.up) * dir;
+                originPoint += (Vector3.right * shotStartWidth) / (shotRays - 1);
+            }
+            shoot();
+            shotCountDown -= Time.deltaTime;
+        }
     }
 
     void shoot()
     {
-        GameObject Bullet = Instantiate(m_projectile, transform.position, transform.rotation);
-        Bullet.GetComponent<BulletScript>().Facing = transform.forward;
-        Bullet.GetComponent<BulletScript>().Velocity = 20;
 
         int layerMask = 1 << 8;
-
         layerMask = ~layerMask;
 
-        RaycastHit hit1;
-        RaycastHit hit2;
-        RaycastHit hit3;
+        RaycastHit[] hit = new RaycastHit[shotRays];
 
-        Vector3 dir = transform.forward;
-        dir.Normalize();
+        Vector3 dir = shotDirection;
+        dir = Quaternion.AngleAxis(-shotEndWidth * 0.5f, Vector3.up) * dir;
 
-        if(!Physics.Raycast(transform.position + (transform.forward * 0.5f), dir, out hit1, 1.0f, layerMask))
+        Vector3 origin = shotOriginPoint + ((-transform.right * shotStartWidth) * 0.5f);
+
+
+        bool enemyHit = false; // this bool only exists for debug reasons at the moment
+        for(int i = 0; i < shotRays; i++)
         {
-            Debug.DrawLine(transform.position + (transform.forward * 0.5f), transform.position + (transform.forward * 0.5f) + dir, Color.green);
-            Debug.Log("Raycast 1 did not hit anything");
+            if(Physics.Raycast(origin, dir, out hit[i], shotDistance, layerMask))
+            {
+                enemyHit = true;
+                Debug.Log("ray " + i + " hit something!");
+                GameObject enemy = hit[i].collider.gameObject;
+
+                if(!hit[i].collider.CompareTag("Player") && hit[i].distance <= (shotDistance * ((shotTime - shotCountDown) / shotTime)))
+                    Destroy(enemy); // tell enemy they've been hit here
+                // optional: compare distance to hit object. e.g. if distance > shotTimer, enemy wasn't hit.
+            }
+            dir = Quaternion.AngleAxis(shotEndWidth / (shotRays - 1), Vector3.up) * dir;
+            origin += (transform.right * shotStartWidth) / (shotRays - 1);
         }
 
-        dir = Quaternion.AngleAxis(45, Vector3.up) * dir;
-        dir.Normalize();
-
-        if (!Physics.Raycast(transform.position + transform.forward * 0.5f, dir, out hit2, 1.0f, layerMask))
+        if (!enemyHit)
         {
-            Debug.Log("Raycast 2 did not hit anything");
-        }
-
-        dir = Quaternion.AngleAxis(-90, Vector3.up) * dir;
-        dir.Normalize();
-
-        if (!Physics.Raycast(transform.position + transform.forward * 0.5f, dir, out hit3, 1.0f, layerMask))
-        {
-            Debug.Log("Raycast 3 did not hit anything");
-        }
-
-        if ( hit1.distance < 0.6f && !hit1.collider.CompareTag("Projectile")
-         || hit2.distance < 0.6f && !hit2.collider.CompareTag("Projectile")
-         || hit3.distance < 0.6f && !hit3.collider.CompareTag("Projectile"))
-        {
-            Debug.Log("hit");
+            Debug.Log("None of the rays hit anything.");
         }
     }
 
@@ -97,19 +123,19 @@ public class CharacterControl : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position += Vector3.forward * speed * Time.deltaTime;
+            transform.position += Vector3.forward * playerSpeed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position += Vector3.back * speed * Time.deltaTime;
+            transform.position += Vector3.back * playerSpeed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position += Vector3.left * speed * Time.deltaTime;
+            transform.position += Vector3.left * playerSpeed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += Vector3.right * speed * Time.deltaTime;
+            transform.position += Vector3.right * playerSpeed * Time.deltaTime;
         }
     }
 }
