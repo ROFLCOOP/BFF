9f5c38fe4cfc784a13d762f39b9a234649e1b7a2
zack <zedoov@hotmail.com>
2019-05-23 13:21:24 +1000

    added wave feature

--- BFF/Assets/Scripts/Spawn.cs
+++ BFF/Assets/Scripts/Spawn.cs
@@ -1,43 +1,94 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Spawn : MonoBehaviour
 {
+    [Header("AI character prefab")]
     public GameObject aIGameObject;
-    public Transform spawnLocation;
+    [Header("Spawn locations")]
+    public Transform[] spawnLocations;
 
+    [Header("Parameters of spawning")]
     public int spawnAmount;
     public int waveMultiplier;
     public float timeBetweenSpawns;
+    public float startTimeBetweenWaves;
+    public float addedTimeBetweenWaves;
+
+    [Header("Countdown timer for next wave")]
+    public Text waveCountdownText;
+
 
     private float timer;
+    private int childNumber;
+
+
 
 
     // Start is called before the first frame update
     void Start()
     {
+        timer = startTimeBetweenWaves;
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
+        timer -= Time.deltaTime;
 
 
-        if (this.transform.childCount == 0/* && other spawns dont have any children*/)
-        {//basically starts a new wave
-            StartCoroutine(spawnDelay());
+
+        if (numberOfChildren() == 0)
+        {
+            waveCountdownText.gameObject.SetActive(true);
+            if (timer < 0)
+            {
+                startTimeBetweenWaves = startTimeBetweenWaves + addedTimeBetweenWaves;
+                if(startTimeBetweenWaves < 0)
+                {
+                    startTimeBetweenWaves = 0;
+                }
+                StartCoroutine(spawnDelay());
+                timer = 0;
+            }
         }
+        else
+        {
+            timer = startTimeBetweenWaves;
+            waveCountdownText.gameObject.SetActive(false);
+        }
+        waveCountdownText.text = timer.ToString("F2");
+    }
 
+    int numberOfChildren()
+    {
+        for (int i = 0; i < spawnLocations.Length; i++)
+        {
+            childNumber = this.transform.childCount;
+        }
+        return childNumber;
+    }
 
+    bool areThereChildren()
+    {
+        if (childNumber > 0)
+        {
+            return true;
+        }
+        else
+            return false;
     }
 
     IEnumerator spawnDelay()
     {
         for (int i = 0; i < (spawnAmount * waveMultiplier); i++)
         {
-            Instantiate(aIGameObject, spawnLocation);
+            int index = Random.Range(0, spawnLocations.Length);
+
+            Instantiate(aIGameObject,spawnLocations[index].position,Quaternion.Euler(0,0,0),this.transform);
+            
             yield return new WaitForSeconds(timeBetweenSpawns);
         }
         waveMultiplier = waveMultiplier + 1;
