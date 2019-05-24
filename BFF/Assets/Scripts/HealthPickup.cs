4e6a4402e9b0e05871ea00cfe8db19c2e30d4859
Andrew Kenny <andrewckenny97@gmail.com>
2019-05-24 14:29:16 +1000

    Health Object Knows About Player

--- BFF/Assets/Scripts/HealthPickup.cs
+++ BFF/Assets/Scripts/HealthPickup.cs
@@ -9,7 +9,7 @@ public class HealthPickup : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Fox");
     }
 
     // Update is called once per frame
